25e53f30ad430970285a44733adc0a26169d4a9b
Goran <gospaso@gmail.com>
2020-06-03 14:28:58 -0400

    Add highlights on input

--- main.js
+++ main.js
@@ -1,83 +1,134 @@
-function username() {
-  let username = document.getElementById("username");
-  let errUsername = document.getElementById("errUsername");
-
-  if (username.value === "" || username.value == null) {
-    username.style.backgroundColor = "red";
-    errUsername.innerHTML = "Please enter valid username";
-  } else {
-    username.style.backgroundColor = "white";
-    errUsername.innerHTML = "";
-  }
-
-  if (username.value.length < 4 || username.value.length > 10) {
-    username.style.backgroundColor = "red";
-    errUsername.innerHTML =
-      "Please enter username that has between 3 and 10 characthers";
-  } else {
-    username.style.backgroundColor = "white";
-    errUsername.innerHTML = "";
-  }
-}
-
-function email() {
-  let email = document.getElementById("email");
-  let errEmail = document.getElementById("errEmail");
-
-  if (email.value === "" || email.value == null) {
-    email.style.backgroundColor = "red";
-    errEmail.innerHTML = "Please enter valid email";
-  } else {
-    email.style.backgroundColor = "white";
-    errEmail.innerHTML = "";
-  }
-
-  if (email.value.length < 10 || email.value.length > 30) {
-    email.style.backgroundColor = "red";
-    errEmail.innerHTML = "Please enter valid email";
-  } else {
-    email.style.backgroundColor = "white";
-    errEmail.innerHTML = "";
-  }
-}
-
-function pwd() {
-  let pwd = document.getElementById("pwd");
-  let errPwd = document.getElementById("errPwd");
-
-  if (pwd.value === "" || pwd.value == null) {
-    pwd.style.backgroundColor = "red";
-    errPwd.innerHTML = "Please enter password";
-  } else {
-    pwd.style.backgroundColor = "white";
-    errPwd.innerHTML = "";
-  }
-
-  if (pwd.value.length < 3 || pwd.value.length > 10) {
-    pwd.style.backgroundColor = "red";
-    errPwd.innerHTML = "Please enter password beetwen 3 and 10 characters";
-  } else {
-    pwd.style.backgroundColor = "white";
-    errPwd.innerHTML = "";
-  }
-}
-
-function cpwd() {
-  let cpwd = document.getElementById("cpwd");
-  let errCpwd = document.getElementById("errCpwd");
-  let pwd = document.getElementById("pwd");
-
-  if (cpwd.value != pwd.value) {
-    cpwd.style.backgroundColor = "red";
-    errCpwd.innerHTML = "Passwords are not the same!";
-  } else {
-    cpwd.style.backgroundColor = "white";
-    errCpwd.innerHTML = "";
-  }
-}
 
 function pretrazi() {
   let pretraga = document.getElementById("pretraga").value;
 
   alert(pretraga);
+}
+
+function highlight () {
+  const highlightsEl = document.querySelector('.highlights')
+  highlightsEl.innerHTML = ''
+
+  const value = document.getElementById('pretraga').value
+  const highlights = highlightSentences([value])
+  highlights.forEach(({ top, left, width, height }) => {
+    const highlightEl = document.createElement('div')
+    Object.assign(highlightEl.style, {
+      top: `${top}px`,
+      left: `${left}px`,
+      width: `${width}px`,
+      height: `${height}px`,
+      position: 'absolute',
+      backgroundColor: 'rgba(0,125,0,0.25)',
+      zIndex: 2,
+    })
+    highlightsEl.appendChild(highlightEl)
+  })
+}
+
+function highlightSentences (sentences) {
+  const highlights = []
+  const body = document.body
+  const contentNodes = gatherTextNodes(body)
+
+  sentences.forEach(sentence => {
+    const range = document.createRange()
+    let rangeSentence = sentence.toLowerCase()
+    let start = false
+
+    contentNodes.forEach(node => {
+      const nodeSentence = node.textContent.toLowerCase()
+      const sentIndex = rangeSentence.indexOf(nodeSentence)
+      const nodeIndex = nodeSentence.indexOf(rangeSentence)
+      const s = rangeSentence
+
+      if (sentIndex > -1) {
+        if (!start) {
+          start = true
+          range.setStart(node, sentIndex)
+        }
+        rangeSentence = rangeSentence.slice(nodeSentence.length)
+      }
+
+      if (nodeIndex > -1) {
+        if (!start) {
+          start = true
+          range.setStart(node, nodeIndex)
+        }
+        range.setEnd(node, nodeIndex + s.length)
+
+        start = false
+        rangeSentence = sentence
+        addHighlight(range)
+      }
+    })
+  })
+
+  return highlights
+
+  function addHighlight (range) {
+    const parentEl = getRangeParent(range)
+    const { x, y } = getOffsets(parentEl)
+    Array.from(range.getClientRects()).forEach(({ top, left, width, height }) => {
+      [top, left] = [top - y, left - x]
+      const highlight = { top, left, width, height }
+      const exists = highlights.reduce((bool, h) => {
+        return bool || isObjEq({ ...h }, highlight)
+      }, false)
+      !exists && highlights.push(highlight)
+    })
+  }
+
+  function getRangeParent (range) {
+    const el = range.commonAncestorContainer
+    if (el.nodeName === '#text') {
+      return el.parentNode
+    }
+    return el
+  }
+
+  function gatherTextNodes (node) {
+    if (node.nodeName !== '#text') {
+      let res = []
+      node.childNodes.forEach(child => {
+        res = res.concat(gatherTextNodes(child))
+      })
+      return res
+    } else {
+      return [node]
+    }
+  }
+
+  function isObjEq (a, b) {
+    var aProps = Object.getOwnPropertyNames(a)
+    var bProps = Object.getOwnPropertyNames(b)
+
+    if (aProps.length !== bProps.length) {
+      return false
+    }
+
+    for (var i = 0; i < aProps.length; i++) {
+      var propName = aProps[i]
+      if (a[propName] !== b[propName]) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  function getOffsets (el) {
+    let x = -(document.documentElement.scrollLeft || document.body.scrollLeft)
+    let y = -(document.documentElement.scrollTop || document.body.scrollTop)
+
+    // el = el.offsetParent
+
+    // while (el) {
+    //   x += (el.offsetLeft - el.scrollLeft + el.clientLeft)
+    //   y += (el.offsetTop - el.scrollTop + el.clientTop)
+    //   el = el.offsetParent
+    // }
+
+    return { x, y }
+  }
 }
